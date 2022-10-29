fa0b8a90a66490cee7213bf822aa7bf37fc86164
Andy Galdi <agaldi@zelusanalytics.com>
2022-10-28 22:22:05 -0400

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by Andy Galdi on 10/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Andy Galdi on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
