940948d04c153695e5595b79fc62da468a241498
Andy Galdi <agaldi@zelusanalytics.com>
2022-10-29 10:02:29 -0400

    Create steppers and pickers for settings view

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gameIsActive = false
+    @State private var selectedNumber = 2
+    @State private var allowedNumQuestions = [5, 10, 20]
+    @State private var selectedNumQuestions = 5
+    @State private var allowedDifficulty = ["Easy", "Medium", "Hard"]
+    @State private var selectedDifficulty = "Easy"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section("Select multiplication table") {
+                    Stepper("\(selectedNumber) times table", value: $selectedNumber, in: 2...12)
+                }
+                
+                Section("Select number of questions") {
+                    Picker("Question", selection: $selectedNumQuestions) {
+                        ForEach(allowedNumQuestions, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Select difficulty") {
+                    Picker("Difficulty", selection: $selectedDifficulty) {
+                        ForEach(allowedDifficulty, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+            .navigationTitle("Multiply")
+        }
     }
 }
 
