ea60a4397baf1a17f0f18424ce6b86ca0b015f78
Andy Galdi <agaldi@zelusanalytics.com>
2022-12-04 20:56:51 -0500

    Add gameIsActive view

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var gameIsActive = false
-    @State private var selectedNumber = 2
-    @State private var allowedNumQuestions = [5, 10, 20]
-    @State private var selectedNumQuestions = 5
-    @State private var allowedDifficulty = ["Easy", "Medium", "Hard"]
-    @State private var selectedDifficulty = "Easy"
-    @State private var questions = [Question]()
+    @State private var gameIsActive: Bool = false
+    @State private var selectedNumber: Int = 2
+    @State private var allowedNumQuestions: Array<Int> = [5, 10, 20]
+    @State private var selectedNumQuestions: Int = 5
+    @State private var allowedDifficulty: Array<String> = ["Easy", "Medium", "Hard"]
+    @State private var selectedDifficulty: String = "Easy"
+    @State private var questions: Array<Question> = []
+    @State private var questionIndex: Int = 0
+    @State private var userAnswer: String = ""
+    @State private var userScore: Int = 0
+    @State private var isGameComplete: Bool = false
+    @State private var gameCompleteMessage: String = ""
     
     struct Question {
         let text: String
@@ -22,57 +27,97 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView{
-            VStack {
-                Form {
-                    Section("Select multiplication table") {
-                        Stepper("\(selectedNumber) times table", value: $selectedNumber, in: 2...12)
+        if gameIsActive {
+            Group {
+                VStack {
+                    Text(questions[questionIndex].text)
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    TextField("Type your answer here", text: $userAnswer)
+                        .keyboardType(.decimalPad)
+                    Spacer()
+                    Button("Enter") {
+                        checkAnswer(givenAnswer: Int(userAnswer) ?? 0, answer: questions[questionIndex].answer)
                     }
-                    
-                    Section("Select number of questions") {
-                        Picker("Question", selection: $selectedNumQuestions) {
-                            ForEach(allowedNumQuestions, id: \.self) {
-                                Text($0, format: .number)
-                            }
-                        }
-                        .pickerStyle(.segmented)
+                    .alert(isPresented: $isGameComplete) {
+                        Alert(title: Text(gameCompleteMessage), message: Text("Restart game"), primaryButton: .destructive(Text("Okay")) {
+                            gameIsActive = false
+                            userAnswer = ""
+                            userScore = 0
+                            gameCompleteMessage = ""
+                            questionIndex = 0
+                        }, secondaryButton: .cancel())
                     }
-                    
-                    Section("Select difficulty") {
-                        Picker("Difficulty", selection: $selectedDifficulty) {
-                            ForEach(allowedDifficulty, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                        .pickerStyle(.segmented)
-                    }
-                    
-                    
                 }
-                .navigationTitle("Multiply")
-                
-                Button("Start Game"
-                      // , action: startGame
-                ) {
-                    gameIsActive.toggle()
+            }
+        } else {
+            Group {
+                VStack {
+                    Form {
+                        Section("Select multiplication table") {
+                            Stepper("\(selectedNumber) times table", value: $selectedNumber, in: 2...12)
+                        }
+                        
+                        Section("Select number of questions") {
+                            Picker("Question", selection: $selectedNumQuestions) {
+                                ForEach(allowedNumQuestions, id: \.self) {
+                                    Text($0, format: .number)
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                        
+                        Section("Select difficulty") {
+                            Picker("Difficulty", selection: $selectedDifficulty) {
+                                ForEach(allowedDifficulty, id: \.self) {
+                                    Text($0)
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                        
+                        
+                    }
+                    .navigationTitle("Multiply")
+                    
+                    Button("Start Game") {
+                        createQuestions()
+                        gameIsActive.toggle()
+                    }
+                    .padding(50)
+                    .background(.green)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
                 }
-                .padding(50)
-                .background(.green)
-                .foregroundColor(.white)
-                .clipShape(Circle())
             }
         }
     }
     
-    func createQuestions(selectedNum: Int, numQuestions: Int, difficulty: String) {
-        questions = [Question]()
-        for _ in 1...numQuestions {
-            let randomInt = Int.random(in: 0..<13)
-            let question = Question(text: "What is \(selectedNum) x \(randomInt)?", answer: selectedNum*randomInt)
+    func createQuestions() {
+        questions.removeAll()
+        for i in 1...selectedNumQuestions {
+            let question = Question(text: "What is \(selectedNumber) x \(i)?", answer: selectedNumber * i)
             questions.append(question)
         }
     }
-}
+    
+    func checkAnswer(givenAnswer: Int, answer: Int) {
+        if answer == givenAnswer {
+            userScore += 1
+        }
+        userAnswer = ""
+        if questionIndex < selectedNumQuestions - 1 {
+            questionIndex += 1
+        } else {
+            gameCompleteMessage = "Your score is \(userScore)"
+            isGameComplete = true
+        }
+    }
+    
+    func startGame() {
+        
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
